c9c5b92edb3e6a87b5992f0edb338e7fbae238ac
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 00:40:56 +0200

    Update settings.js for Edge234

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;7ZdxXCrb#5RrQccZwJR9f7TkQalzitaIrGXxP4stfYlLLjJWVv1Q",
+  OWNER_NUMBER: "+237693733869", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
