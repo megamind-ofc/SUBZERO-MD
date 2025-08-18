5a7e66048743fc54b351ddd558a2ca7c754dfec3
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:06:26 +0200

    Update settings.js for Queen-Anita

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;Sd0WzRTI#OpRMbj509iB_Wvh_SvAq98OnLNV5ibpYqHDC4EH8sVs",
+  OWNER_NUMBER: "2347044745808", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
