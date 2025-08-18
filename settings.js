b8a25125610b8faa6480795b6f8da492da100f62
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 18:17:11 +0200

    Update settings.js for Xavage

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;6Acw0KRD#xD0-J1A6_gEiMdF51deTE46N-j1LU0UxdDKzSba-Cl8",
+  OWNER_NUMBER: "+256742728768", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
