aa3146f43fe6e092c697aeb36ed20140e3957fad
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:45:21 +0200

    Update settings.js for Zxxsjd

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;mAEDybrL#d_9yLHnw_uhso81X2z6zZDr5-2V0U2_G_Vk7zC6CcJg",
+  OWNER_NUMBER: "+2349126808491", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
