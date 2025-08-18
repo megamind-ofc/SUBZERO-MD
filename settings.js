7d4328804e446e1a595cc0e8f22b787664c53d35
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:05:31 +0200

    Update settings.js for my-barracks-bot

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;TNFwVJyJ#u2vTHYSRR4FqJqTrARH4n-5nTolwp9Az7Jp_LDrHkQ4",
+  OWNER_NUMBER: "+2567596 05571 ", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
