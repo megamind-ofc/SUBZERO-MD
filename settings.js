e126691aa6bb54e8c9b4247ef6deff2e02686b4c
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:11:24 +0200

    Update settings.js for Nexus-Ai

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;LV1g0SZa#ZDunWrPUEma-3fBiiN0OuyVncg1zODI-_1jRIeBKnb0",
+  OWNER_NUMBER: "2347044745808", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
