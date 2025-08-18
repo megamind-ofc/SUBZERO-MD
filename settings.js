e6d03ce906fbe15baa9b29521bfe36918e2fcbde
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:18:41 +0200

    Update settings.js for gladdybot

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;zUUEjbDB#wSSE6kZii1mHs8yDvRC6pTA_eIW1tH2zq4r7Npcf_6Q",
+  OWNER_NUMBER: "+263775953409", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
