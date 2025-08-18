620fab4b215ac225424caf9cba5b811d764d3287
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 15:26:43 +0200

    Update settings.js for Jafar

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;yYkkEbTB#7ytLB3WHomjMV8BMElZKGEZ_gNbyrrdf8Vv-BYtYEPc",
+  OWNER_NUMBER: "2347065831011", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
