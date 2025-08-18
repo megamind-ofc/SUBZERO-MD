668f35f705a9f409278301866deb7c54969392f9
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 19:07:34 +0200

    Update settings.js for PTABIN

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;ecd0UQLb#n06Y_HvPs8vqqLm8s74r5B4sfads4w_Eb73-LuXqqlY",
+  OWNER_NUMBER: "9883960488", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
