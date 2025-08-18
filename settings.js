c2655703115a06058685c6927611bbe25956ff4b
megamind-ofc <dyanamictech@gmail.com>
2025-08-18 16:57:09 +0200

    Update settings.js for Dev7

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;iNkmFL7Q#jhao3ZSZ7Mn99bth9Pj3_HTGu53CXoZ87ye-diOzEq0",
+  OWNER_NUMBER: "+6285142289862", 
+  PREFIX: ".start"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
