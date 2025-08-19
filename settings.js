b2c3c25352532b72b507e10511ba83db1e493ab3
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 20:47:34 +0200

    Update settings.js for Boyytttg

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;OIM3ULLT#LY7cZdLyn4epZmsQUR95xkzIPim66mL0-Lxs0recmBA",
+  OWNER_NUMBER: "+27604052050", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
