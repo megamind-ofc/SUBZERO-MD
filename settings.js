6ed2885467b213eb4c0db0487fbeee4368ce15be
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 07:21:47 +0200

    Update settings.js for Sergeant-manny

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;eQlx3a4J#jXPSzHaN1Yd2ZfhXf0akDOHasD-11LHuwvBWwOi-_AM",
+  OWNER_NUMBER: "+254113706692", 
+  PREFIX: "!"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
