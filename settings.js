37ff4452f1961ff1d03dab243a44c06d5880bef4
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 20:54:43 +0200

    Update settings.js for PSYCHO-MD

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;SBs1yJyT#3fqCDfdEDJQhuKizNC6LME51TahWDGMFfNInA0KTczQ",
+  OWNER_NUMBER: "94721243143", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
