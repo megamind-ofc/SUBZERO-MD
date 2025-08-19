ea38b66e791d0f8eed21517e6ea2bf3b616427c9
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 15:07:19 +0200

    Update settings.js for Java02

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;fNk2WJSQ#BhTsy_C7RqGhKGnlsaaMV-6txgo3ZsIge1cTXuNCL64",
+  OWNER_NUMBER: "078553 3648", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
