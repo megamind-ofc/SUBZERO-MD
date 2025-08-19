7316f80094588f0f8a456e42418a19feaaf9a57d
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 22:16:32 +0200

    Update settings.js for Yule

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;idkDHbYa#Oy0xPopI-_JzMJdQ8m3iGVcpjA7wf4fuUcM7UrLqrVU",
+  OWNER_NUMBER: "+255672222616", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
