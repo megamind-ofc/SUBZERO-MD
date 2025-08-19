6103cab22d18f02a4b4d05bf8a6c40a3f4fa5b64
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 11:07:53 +0200

    Update settings.js for Savage

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;nEFC0agR#NBQyAOiFfiGF_AaldZyZIFIEzurR7dMjzpqfm9XvWs4",
+  OWNER_NUMBER: "+256784079946", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
