293a26e40af33cc27653dc8369c23c302a3ad364
megamind-ofc <dyanamictech@gmail.com>
2025-08-19 20:25:46 +0200

    Update settings.js for Growth1

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;eRE3gLpa#XV0rjccB3xtMRNOpyQvFVMsVMW9Eemf_d7q-E7p9DAA",
+  OWNER_NUMBER: "23407048625339", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
