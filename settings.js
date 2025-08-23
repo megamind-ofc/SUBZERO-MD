8657e321ca66b49074e9b80d4772b819fc9d3b03
megamind-ofc <dyanamictech@gmail.com>
2025-08-23 06:22:17 +0200

    Update settings.js for Jon-Loner

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;KdlSSAAJ#ow4afRLj4Bpgd4oDX1Ohctw1cn307ENQ9MIz6VQn_Gs",
+  OWNER_NUMBER: "+2349031929027", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
