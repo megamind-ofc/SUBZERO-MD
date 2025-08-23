42c4168fdb0ce16fe9a94f2fba159a4701425998
megamind-ofc <dyanamictech@gmail.com>
2025-08-23 11:24:26 +0200

    Update settings.js for Najam-Shehzad-GH-lL

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;eZVB1ZwR#Qbdngp88GN2AxN9cpuE4e4qvcwI6kz9Bi8WwnaY7i4Q",
+  OWNER_NUMBER: "923480772867", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
