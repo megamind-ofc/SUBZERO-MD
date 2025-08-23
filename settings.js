f2c2b8a804b8cec7e34e8e1acbca045474901f7f
megamind-ofc <dyanamictech@gmail.com>
2025-08-23 17:15:48 +0200

    Update settings.js for collins

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;SMNA0KiC#ck0x11aoU2JsioiZUvmiJZmGCWcU4NEUwF9t6eie0BM",
+  OWNER_NUMBER: "263", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
