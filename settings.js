d517cd5d893be5a49bb90f40b1eb619e4e2a03a5
megamind-ofc <dyanamictech@gmail.com>
2025-08-23 09:03:12 +0200

    Update settings.js for Fiftyyuuuuu

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;KA8xAZxZ#gWGns3tOI1GHAv9RGNmEfC4IIWt4N5D-Ab5ePU0BL_o",
+  OWNER_NUMBER: "2348169045105", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
