aba8428f40d5432cdc0457847643015965593f18
megamind-ofc <dyanamictech@gmail.com>
2025-09-20 14:34:52 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~630db953cc336f2864b1486af872c319fe8e6bb5", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
