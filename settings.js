edea8e749b66dca2ebdd57fb41538fe7de3c96a3
megamind-ofc <dyanamictech@gmail.com>
2025-10-12 20:15:31 +0200

    Update settings.js for subzeroyyyyx

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~f156fd490f2ddb506fd2e30b81879254ad001f5f",
+  OWNER_NUMBER: "263719647303 ", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
